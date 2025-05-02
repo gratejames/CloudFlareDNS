5061d397d3eb7d92e7cb6211c965aabb4c00c529
GrateJames <jimmy.l.smythe@gmail.com>
2025-05-02 13:21:13 -0600

    Don't need sudo for status

--- CloudFlareDNS.py
+++ CloudFlareDNS.py
@@ -7,14 +7,19 @@ parser = argparse.ArgumentParser(prog="cf", description="Simple CloudFlare DNS T
 
 parser.add_argument("command", help="command to issue", choices=["enable", "disable", "toggle", "status"], nargs="?")
 parser.add_argument("-s", "--silent", help="silences all printing", action="store_true")
+parser.add_argument("-c", "--color", help="Highlight display", action="store_true")
+parser.add_argument("-i", "--indent", help="Indent display", action="store_true")
 args = parser.parse_args()
 
 enabled = False
 ignoreAuto = "no"
 
+# TODO: Fetch this and not need it as a const
+networkName = "Mines-Legacy"
+
 def load():
 	global enabled, ignoreAuto
-	con = nmcli.connection.show("Mines")
+	con = nmcli.connection.show(networkName)
 	ignoreAuto = con["ipv4.ignore-auto-dns"]
 	i = 1
 	DNS = []
@@ -26,10 +31,10 @@ def load():
 
 def downUpCon():
 	try:
-		nmcli.connection.down("Mines")
+		nmcli.connection.down(networkName)
 	except nmcli._exception.NotExistException:
 		pass
-	nmcli.connection.up("Mines")
+	nmcli.connection.up(networkName)
 
 def enable():
 	global enabled
@@ -38,8 +43,8 @@ def enable():
 		if not args.silent:
 			print("Already enabled, exiting")
 		exit()
-	nmcli.connection.modify("Mines", {"ipv4.ignore-auto-dns": "yes"})
-	nmcli.connection.modify("Mines", {"ipv4.dns": "1.1.1.1 1.0.0.1"})
+	nmcli.connection.modify(networkName, {"ipv4.ignore-auto-dns": "yes"})
+	nmcli.connection.modify(networkName, {"ipv4.dns": "1.1.1.1 1.0.0.1"})
 	downUpCon()
 
 def disable():
@@ -49,8 +54,8 @@ def disable():
 		if not args.silent:
 			print("Already disabled, exiting")
 		exit()
-	nmcli.connection.modify("Mines", {"ipv4.ignore-auto-dns": "no"})
-	nmcli.connection.modify("Mines", {"ipv4.dns": ""})
+	nmcli.connection.modify(networkName, {"ipv4.ignore-auto-dns": "no"})
+	nmcli.connection.modify(networkName, {"ipv4.dns": ""})
 	downUpCon()
 
 if args.command == "enable":
@@ -64,7 +69,15 @@ if args.command == "toggle":
 		enable()
 
 if not args.silent:
+	nmcli.disable_use_sudo()
 	load()
-	print("Status:")
-	print(f"- Ignore DHCP DNS:    {ignoreAuto}")
-	print(f"- CloudFlare enabled: {'yes' if enabled else 'no'}")
+	indentChar = " " if args.indent else ""
+	if args.color:
+		ignoreAutoColor = '\033[32myes\033[0m' if (ignoreAuto == 'yes') else '\033[31mno\033[0m'
+		print(f"{indentChar}Ignore DHCP DNS:    {ignoreAutoColor}")
+		enabledColor = '\033[32myes\033[0m' if enabled else '\033[31mno\033[31m'
+		print(f"{indentChar}CloudFlare enabled: {enabledColor}")
+	else:
+		print(f"{indentChar}Ignore DHCP DNS:    {ignoreAuto}")
+		print(f"{indentChar}CloudFlare enabled: {'yes' if enabled else 'no'}")
+
